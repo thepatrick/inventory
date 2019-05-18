20a5d7414c16a6c6761f9178bb1a660372fcc7df
Patrick Quinn-Graham <make-contact@pftqg.com>
2019-05-18 22:09:16 +1000

    Initial commit of an ansible setup for my servers (#3)
    * Initial commit of an ansible setup for my servers
    
    * Replace geerlingguy.github-users with my own users role

---
+++ servers/ansible-galaxy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ansible-galaxy install --roles-path `pwd`/roles -r requirements.yml
---
+++ servers/ansible-playbook.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -euf
+
+if [ "$#" -lt 1 ]; then
+    echo "You must specify at least a limit (e.g. ansible-up.sh hostname)"
+    exit
+fi
+
+# TODO: clone PLAYBOOK_REPO if not present, git pull if it is.
+#PLAYBOOK_REPO=../../debconf-ansible
+#PLAYBOOKS="$PLAYBOOK_REPO/site.yml site.yml"
+PLAYBOOKS="site.yml"
+
+LIMIT_FILTER=$1
+shift
+
+ANSIBLE_USER=${ANSIBLE_USER:-patrick}
+
+ansible-playbook \
+    --diff \
+    --inventory-file=hosts \
+    --become \
+    --user $ANSIBLE_USER \
+    --limit=$LIMIT_FILTER \
+    $PLAYBOOKS \
+    "$@"
